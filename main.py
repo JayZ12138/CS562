62ae6302bdc7f220016ab151a84f1c02ffebe357
Hanjie <hanjie@Hanjies-MacBook-Pro.local>
2017-04-29 13:01:19 -0400

    add interface

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from snapshot import Snapshot
+import sys
+import time
 
 def main():
 	fname = 'snapshot_test.txt'
+	print('Start reading and indexing file...')
+	start = time.time()
 	with open(fname, 'r') as f:
 		line = f.readline().split()
 		Sindex = Snapshot([line[1], int(line[0]), '*'])
@@ -12,16 +16,36 @@ def main():
 				Sindex.insert([line[1], int(line[0]), '*'])
 			elif(op == 'd'):
 				Sindex.delete(line[1], int(line[0]))
-	# print(Sindex.blocks.SP.next_node.block.id)
-	# print(Sindex.blocks.SP.next_node.Pce_node.block.record_list)
-	# print(Sindex.AT)
-	# print(Sindex.alives_entries)
-	# print(Sindex.tsquery(5))
-	s = Sindex.tsquery(5)|Sindex.tsquery(6)|Sindex.tsquery(7)
-	s = sorted(list(s))
-	y = sorted(list(Sindex.trquery(5,7)))
-	print(s)
-	print(y)
-	print(s==y)
+	duration = time.time() - start
+	print('')
+	print('Index done in: {0:.2f}s '.format(duration))
+	print('')
+	print('Enter the query type (ts or tr) and time')
+	print('timeslice query format: ts time_instance')
+	print('timerrange query format: tr min_time max_time')
+	print('Enter anything else to quit')
+	print('')
+	for line in sys.stdin:
+		qtype, *t = line.split()
+		if(qtype == 'ts'):
+			start = time.time()
+			result = Sindex.tsquery(int(t[0]))
+			duration = time.time() - start
+		elif(qtype == 'tr'):
+			start = time.time()
+			result = Sindex.trquery(int(t[0]), int(t[1]))
+			duration = time.time() - start
+		else:
+			quit()
+		print('result: ',result)
+		print('')
+		print('Get time result in {0:.5f}s'.format(duration))
+
+	# s = Sindex.tsquery(5)|Sindex.tsquery(6)|Sindex.tsquery(7)
+	# s = sorted(list(s))
+	# y = sorted(list(Sindex.trquery(5,7)))
+	# print(s)
+	# print(y)
+	# print(s==y)
 if __name__=='__main__':
 	main()
